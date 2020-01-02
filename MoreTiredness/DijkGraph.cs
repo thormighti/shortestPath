b2cd934aa4294321c55bc143ae5bd460cd98866a
Abeshi Peter Unimke <peterabeshi97@outlook.com>
2020-01-02 14:34:21 +0100

    Add project files.

---
+++ MoreTiredness/DijkGraph.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreTiredness
+{
+    class DijkGraph
+    {
+        int vertice;
+        int[,] list;
+        public DijkGraph(int vertice)
+        {
+            this.vertice = vertice;
+            list = new int[vertice, vertice];
+        }
+        public void AddEdge(int source, int destination, int weight)
+        {
+            list[source,destination] = weight;
+        }
+        int MininimumDistance(int [] dist, bool [] isIncluded)
+        {
+            int min = int.MaxValue;
+            int mindIndex = 0;
+            for (int i = 0; i < vertice; i++)
+            {
+                if(isIncluded[i] == false && min > dist[i])
+                {
+                    min = dist[i];
+                    mindIndex = i;
+                }
+
+            }
+            return mindIndex;
+        }
+       public void ShortPath(int source)
+        {
+            int[] dist = new int[vertice]; // holds distance of current path
+            bool[] isIncluded = new bool[vertice]; // checks if its included in the shortest path
+
+            // initializong the distances to infinity and included as false
+            for (int i = 0; i < vertice; i++)
+            {
+                dist[i] = int.MaxValue;
+                
+            }
+            // initial distance
+            dist[source] = 0; // of course you suppose be zero na
+            // now finding the shortest path 
+            for (int v = 0; v < vertice ; v++)
+            {
+                // getting the minimum node from node not yet processed
+                int u = MininimumDistance(dist, isIncluded);
+                isIncluded[u] = true; // mark the vertex as included
+                // update 
+                for (int node = 0; node < vertice; node++)
+                {
+                    // check the edges between U and node
+                    if (list[u, node] > 0)
+                    {
+                        // check if node is already included and dist value is infinity
+                        if (isIncluded[node] == false && list[u, node] != int.MaxValue)
+                        {
+                            // update the dist value if its not in set
+                            if (dist[u] + list[u, node] < dist[node])
+                            {
+                                // update
+                                dist[node] = dist[u] + list[u, node];
+                            }
+                        }
+                    }
+                }
+            }
+            PrintShow(source,dist);
+        }
+        void PrintShow(int source,int [] n)
+        {
+           // Console.Write(" distance from vertex  ");
+            for (int i = 0; i < vertice; i++)
+            {
+                Console.WriteLine("Source node " + source + " To vertex " + i + " is "+ n[i]);
+            }
+        } 
+    }
+}
---
+++ MoreTiredness/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoreTiredness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for directed graph
+            DijkGraph graph = new DijkGraph(5);
+            graph.AddEdge(0, 1, 10);
+            graph.AddEdge(0, 3, 7);
+            graph.AddEdge(1, 3, 8);
+            graph.AddEdge(1, 2, 5);
+            graph.AddEdge(1, 4, 2);
+            graph.AddEdge(2, 4, 8);
+            graph.AddEdge(3, 4, 6);
+            //graph.AddEdge(0, 1, 4);
+            //graph.AddEdge(0, 2, 3);
+            //graph.AddEdge(1, 2, 1);
+            //graph.AddEdge(1, 3, 2);
+            //graph.AddEdge(2, 3, 4);
+            //graph.AddEdge(3, 4, 2);
+            //graph.AddEdge(4, 5, 6);
+            graph.ShortPath(0);
+
+        }
+    }
+}
